71453f7186f5a52c455c88c1559652690bb81da2
Narendra Kumar <nk7666324@gmail.com>
2026-07-12 17:59:31 +0530

    create file and write StudentName

--- File_handling.cpp
+++ File_handling.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
-#include<fstream>
+#include <fstream>
+#include<string>
 using namespace std;
 int main(){
-    // ofstream  file("main.txt", ios::app);
-    // file.close();
-    // cout<<"File created successfully";
-    // fstream file("student.txt", ios::out);
-    // file.close();
-    // cout<<"File data deleted successfully";
-    
-    return 0;
+fstream file("Profile.txt");
+if(!file){
+    cout<<"File not created";
+}
+else{
+    cout<<"File created successfully";
+}
+cout<<"Enter the Name:"<<endl;
+string studentName;
+cin>>studentName;
+file<<"Student_Name\n"<<studentName<<endl;
+file.close();
+cout<<"data write"<<endl;
+return 0;
 }
