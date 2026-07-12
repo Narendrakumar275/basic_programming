050b50d904f7bd90d992aa1bb3b0a79af0844c38
Narendra Kumar <nk7666324@gmail.com>
2026-07-12 16:35:43 +0530

    Added file handling program

---
+++ File_handling.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    // ofstream  file("main.txt", ios::app);
+    // file.close();
+    // cout<<"File created successfully";
+    // fstream file("student.txt", ios::out);
+    // file.close();
+    // cout<<"File data deleted successfully";
+    
+    return 0;
+}
