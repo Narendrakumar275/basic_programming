9fe4e19a76bc8e8e475a5a09627edab5703dcb3a
Narendra Kumar <nk7666324@gmail.com>
2026-07-13 13:23:06 +0530

    file create(Profile.txt)

--- File_handling.cpp
+++ File_handling.cpp
@@ -1,20 +1,15 @@
 #include<iostream>
 #include <fstream>
-#include<string>
+// #include<string>
 using namespace std;
 int main(){
-fstream file("Profile.txt");
+fstream file("Profile.txt",ios::out);
 if(!file){
     cout<<"File not created";
 }
 else{
     cout<<"File created successfully";
 }
-cout<<"Enter the Name:"<<endl;
-string studentName;
-cin>>studentName;
-file<<"Student_Name\n"<<studentName<<endl;
 file.close();
-cout<<"data write"<<endl;
 return 0;
 }
